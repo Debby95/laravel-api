34c9e6d967e0d6e91ecd704d9cfa846c65f069c1
Debby95 <debbyjacksonstar@hotmail.it>
2022-08-29 21:49:38 +0200

    img added

--- resources/js/frontend/components/PostsList.vue
+++ resources/js/frontend/components/PostsList.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="row row-cols-3">
         <div class="col" v-for="post in posts" :key="posts.id">
-            <div class="card" style="width: 18rem;">
-                <img :src="post.cover_img" class="card-img-top" alt="...">
+            <div class="card mb-3" style="width: 18rem;">
+                <img :src="getImageSrc(post)" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">{{post.title}}</h5>
                     <p class="card-text" v-html="post.content"></p>
@@ -28,10 +28,23 @@ import axios from 'axios';
                 .then((resp) => {
                     this.posts = resp.data
                 })
-            }
+            },
+            getImageSrc(post) {
+                if (!post.cover_img) {
+                    return "/img/img-not-available.jpg";
+                }
+                return post.cover_img;
+            },
         },
         mounted() {
             this.fetchPosts();
         }
     }
 </script>
+
+<style>
+    .card-img-top {
+        aspect-ratio: 16/9;
+        object-fit: cover;
+    }
+</style>
